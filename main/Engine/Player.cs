d30240f42bcec4c47cd254cbc9f116d5cb8266ad
james.hester@dynavistics.com <james.hester@dynavistics.com>
2020-11-13 16:07:26 -0600

    First Commit

---
+++ main/Engine/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class Player : LivingCreature
+    {
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
+
+        public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencepoints, int level) : base(currentHitPoints, maximumHitPoints)
+        {
+            Gold = gold;
+            ExperiencePoints = ExperiencePoints;
+            Level = level;
+
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
+        }
+    }
+}
---
+++ main/README.md
@@ -0,0 +1,2 @@
+# cSharpRPG
+Learning C# by creating an old school RPG.
